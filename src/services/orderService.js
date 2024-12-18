6459b88fcfe94665a873220701458064264e2c69
Trunpy28 <127753383+Trunpy28@users.noreply.github.com>
2024-12-18 11:32:31 +0700

    add orderService

---
+++ src/services/orderService.js
@@ -0,0 +1,40 @@
+import axiosJWT from "./axiosJWT";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const orderService = {
+    createOrder: async (accessToken) => {
+        const response = await axiosJWT.post(`${apiUrl}/order/create`, {}, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    },
+    getAllOrders: async (accessToken) => {
+        const response = await axiosJWT.get(`${apiUrl}/order/get-all`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    },
+    changeOrderStatus: async (accessToken, orderId, data) => {
+        const response = await axiosJWT.post(`${apiUrl}/order/change-status/${orderId}`, { data }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    },
+    deleteOrder: async (accessToken, orderId) => {
+        const response = await axiosJWT.delete(`${apiUrl}/order/delete/${orderId}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    }
+}
+
+export default orderService;
