ab47dde979aa2758ebda3bb18a7bb13c436110f4
Trunpy28 <127753383+Trunpy28@users.noreply.github.com>
2024-12-21 17:15:15 +0700

    add route manage order

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -3,12 +3,16 @@ import axiosJWT from "./axiosJWT";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const orderService = {
-    createOrder: async (accessToken) => {
-        const response = await axiosJWT.post(`${apiUrl}/order/create`, {}, {
+    createOrder: async (accessToken, shippingInfo, paymentMethod) => {  
+        const response = await axiosJWT.post(`${apiUrl}/order/create`, {
+            shippingInfo,
+            paymentMethod
+        }, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         });
+
         return response.data;
     },
     getAllOrders: async (accessToken) => {
