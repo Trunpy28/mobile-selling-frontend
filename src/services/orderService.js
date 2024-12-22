502be3a2aa644d85a741cd59b3235c2e42b66589
tunguyen02 <tu700131@gmail.com>
2024-12-22 16:03:55 +0700

    add orderCount

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -22,7 +22,21 @@ const orderService = {
             console.error("Tạo đơn hàng thất bại:", error);
             throw error;
         }
-    }
+    },
+
+    countOrders: async () => {
+        try {
+            const response = await axiosJWT.get(`${apiUrl}/order/count`, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(localStorage.getItem("access_token"))}`,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Lấy số lượng đơn hàng thất bại:", error);
+            throw error;
+        }
+    },
 };
 
 export default orderService;
